1fcec09244389c7fe9d4152129f24565a4c9771f
ignaciopeniadg <ignaciopeniadg@gmail.com>
2023-07-06 02:27:49 -0300

    agregado de experiencia

--- src/pages/Estudios/Estudios.jsx
+++ src/pages/Estudios/Estudios.jsx
@@ -3,101 +3,84 @@ import './estudios.css'
 
 function Estudios() {
   return (
-    <section id='estudios'>
-    <h5>Leveleando mis</h5>
-    <h2>Skills</h2>
+    <section id='experiencia'>
+      <h5>Repaso sobre mi</h5>
+      <h2>Experiencia</h2>
 
-    <div className='container skills__container'>
-      <div className='skills__design'>
-        <h3>Diseño Gráfico</h3>
-        <div className='skills__contenido'>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              
-              <h4>Illustrator</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              <h4>Photoshop</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              <h4>InDesign</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              <h4>Premiere Pro</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              <h4>After Effects</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
+      <div className='container experiencia__container'>
+        <div className='experiencia__contenido'>
+          <div className='experiencia__nombre'>
+            <h4 id='experiencia__nombre-puesto'>Director de Arte Sr.</h4>
+            <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
+            <h5 id='experiencia__nombre-fecha'>May 2022 - Actualidad</h5>
+          </div>
+          <div className='experiencia__descripcion'>
+            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+          </div>
         </div>
-      </div>
-      <div className='skills__frontend'>
-        <h3>Desarrollo Front-End</h3>
-        <div className='skills__contenido'>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              <h4>HTML</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              <h4>CSS</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              <h4>Bootstrap</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              <h4>SASS</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              <h4>JavaScript</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
-          <article className='skills__detalle'>
-            <div className='skills__nombre'>
-              <h4>React Js</h4>
-            </div>
-            <div className='skills__estrellas'>
-            </div>
-          </article>
+        <div className='experiencia__contenido'>
+          <div className='experiencia__nombre'>
+            <h4 id='experiencia__nombre-puesto'>Diseñador Gráfico E-Commerce Ssr.</h4>
+            <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
+            <h5 id='experiencia__nombre-fecha'>May 2022 - Actualidad</h5>
+          </div>
+          <div className='experiencia__descripcion'>
+            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+          </div>
+          <div className='experiencia__programas'>
+          </div>
         </div>
+        <div className='experiencia__contenido'>
+          <div className='experiencia__nombre'>
+            <h4 id='experiencia__nombre-puesto'>Sr. Art Director</h4>
+            <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
+            <p id='experiencia__nombre-fecha'>May 2022 - Actualidad</p>
+          </div>
+          <div className='experiencia__descripcion'>
+            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+          </div>
+          <div className='experiencia__programas'>
+          </div>
+        </div>
+        <div className='experiencia__contenido'>
+          <div className='experiencia__nombre'>
+            <h4 id='experiencia__nombre-puesto'>Sr. Art Director</h4>
+            <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
+            <p id='experiencia__nombre-fecha'>May 2022 - Actualidad</p>
+          </div>
+          <div className='experiencia__descripcion'>
+            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+          </div>
+          <div className='experiencia__programas'>
+          </div>
+        </div>
+        <div className='experiencia__contenido'>
+          <div className='experiencia__nombre'>
+            <h4 id='experiencia__nombre-puesto'>Sr. Art Director</h4>
+            <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
+            <p id='experiencia__nombre-fecha'>May 2022 - Actualidad</p>
+          </div>
+          <div className='experiencia__descripcion'>
+            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+          </div>
+          <div className='experiencia__programas'>
+          </div>
+        </div>
+        <div className='experiencia__contenido'>
+          <div className='experiencia__nombre'>
+            <h4 id='experiencia__nombre-puesto'>Sr. Art Director</h4>
+            <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
+            <p id='experiencia__nombre-fecha'>May 2022 - Actualidad</p>
+          </div>
+          <div className='experiencia__descripcion'>
+            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+          </div>
+          <div className='experiencia__programas'>
+          </div>
+        </div>
+        
       </div>
-    </div>
-  </section>
+    </section>
   )
 }
 
--- src/pages/Experiencia/Experiencia.jsx
+++ src/pages/Experiencia/Experiencia.jsx
@@ -33,85 +33,74 @@ function Experiencia() {
           <div className='experiencia__nombre'>
             <h4 id='experiencia__nombre-puesto'>Diseñador Gráfico E-Commerce Ssr.</h4>
             <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
-            <h5 id='experiencia__nombre-fecha'>May 2022 - Actualidad</h5>
+            <h5 id='experiencia__nombre-fecha'>Nov 2021 - May 2022</h5>
           </div>
           <div className='experiencia__descripcion'>
-            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+            <p>Adaptación de piezas digitales para retailers e-commerce, desarrollo de cratividades para campañas. </p>
           </div>
           <div className='experiencia__programas'>
             <SiAdobeillustrator className='iconos__experiencia' />
             <SiAdobephotoshop className='iconos__experiencia' />
-            <SiAdobeindesign className='iconos__experiencia' />
-            <SiAdobepremierepro className='iconos__experiencia' />
             <SiAdobeaftereffects className='iconos__experiencia' />
           </div>
         </div>
         <div className='experiencia__contenido'>
           <div className='experiencia__nombre'>
-            <h4 id='experiencia__nombre-puesto'>Sr. Art Director</h4>
-            <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
-            <p id='experiencia__nombre-fecha'>May 2022 - Actualidad</p>
+            <h4 id='experiencia__nombre-puesto'>Diseñador Gráfico</h4>
+            <h5 id='experiencia__nombre-empresa'>Felices las Vacas</h5>
+            <p id='experiencia__nombre-fecha'>Jul 2021 - Oct 2021</p>
           </div>
           <div className='experiencia__descripcion'>
-            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+            <p>Desarrollo de KV  y adaptación de piezas digitales para redes sociales. Armado de Packaging para productos en formato digital e impreso. Preparación de archivos para ploteo vehicular. Preparación de archivos para cartelería publicitaria. Preparación de archivos para comunicación interna. </p>
           </div>
           <div className='experiencia__programas'>
             <SiAdobeillustrator className='iconos__experiencia' />
             <SiAdobephotoshop className='iconos__experiencia' />
-            <SiAdobeindesign className='iconos__experiencia' />
-            <SiAdobepremierepro className='iconos__experiencia' />
-            <SiAdobeaftereffects className='iconos__experiencia' />
           </div>
         </div>
         <div className='experiencia__contenido'>
           <div className='experiencia__nombre'>
-            <h4 id='experiencia__nombre-puesto'>Sr. Art Director</h4>
-            <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
-            <p id='experiencia__nombre-fecha'>May 2022 - Actualidad</p>
+            <h4 id='experiencia__nombre-puesto'>Diseñador Gráfico Jr</h4>
+            <h5 id='experiencia__nombre-empresa'>Back to Blank</h5>
+            <p id='experiencia__nombre-fecha'>Nov 2020 - Jun 2021</p>
           </div>
           <div className='experiencia__descripcion'>
-            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+            <p>Desarrollo de piezas gráficas para redes sociales, logotipos, implementación de marca y MockUps de productos. Desarrollo de paginas web en WordPressm Elemmentor.</p>
           </div>
           <div className='experiencia__programas'>
             <SiAdobeillustrator className='iconos__experiencia' />
             <SiAdobephotoshop className='iconos__experiencia' />
-            <SiAdobeindesign className='iconos__experiencia' />
-            <SiAdobepremierepro className='iconos__experiencia' />
-            <SiAdobeaftereffects className='iconos__experiencia' />
           </div>
         </div>
         <div className='experiencia__contenido'>
           <div className='experiencia__nombre'>
-            <h4 id='experiencia__nombre-puesto'>Sr. Art Director</h4>
-            <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
-            <p id='experiencia__nombre-fecha'>May 2022 - Actualidad</p>
+            <h4 id='experiencia__nombre-puesto'>Diseñador Gráfico Jr</h4>
+            <h5 id='experiencia__nombre-empresa'>Estimulo Visual</h5>
+            <p id='experiencia__nombre-fecha'>Ago 2017 - Ago 2018</p>
           </div>
           <div className='experiencia__descripcion'>
-            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+            <p>Encargado de diseño para ploteo vehicular. Manejo de Software de diseño, plotter de corte e impresion de archivos. Presentación de bocetos a cliente. </p>
           </div>
           <div className='experiencia__programas'>
             <SiAdobeillustrator className='iconos__experiencia' />
             <SiAdobephotoshop className='iconos__experiencia' />
-            <SiAdobeindesign className='iconos__experiencia' />
-            <SiAdobepremierepro className='iconos__experiencia' />
-            <SiAdobeaftereffects className='iconos__experiencia' />
           </div>
         </div>
         <div className='experiencia__contenido'>
           <div className='experiencia__nombre'>
-            <h4 id='experiencia__nombre-puesto'>Sr. Art Director</h4>
-            <h5 id='experiencia__nombre-empresa'>Oliver, U-Studio</h5>
-            <p id='experiencia__nombre-fecha'>May 2022 - Actualidad</p>
+            <h4 id='experiencia__nombre-puesto'>Asistente de Materia</h4>
+            <h5 id='experiencia__nombre-empresa'>Universidad de Palermo</h5>
+            <p id='experiencia__nombre-fecha'>May 2022 - Jul 2018</p>
           </div>
           <div className='experiencia__descripcion'>
-            <p>Desarrollo de KV  y adaptación de piezas digitales para diferentes campañas en marcas específicas. Dirección de Arte para cuenta Axe Latam @axe.latam, desarrollo de línea gráfica en productos para la cuenta, creación de contenido visual. </p>
+            <p>Reflexión Artística</p>
+            <p>Producción Gráfica</p>
+            <p>Tipografía</p>
           </div>
           <div className='experiencia__programas'>
             <SiAdobeillustrator className='iconos__experiencia' />
             <SiAdobephotoshop className='iconos__experiencia' />
             <SiAdobeindesign className='iconos__experiencia' />
-            <SiAdobepremierepro className='iconos__experiencia' />
-            <SiAdobeaftereffects className='iconos__experiencia' />
           </div>
         </div>
         
