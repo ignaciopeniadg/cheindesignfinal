7a4dfe9f80f331d22423a74f4874199fd1d70742
ignaciopeniadg <ignaciopeniadg@gmail.com>
2023-07-06 03:17:48 -0300

    intento de animaciones, no funciona.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,52 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
-import {Home} from './pages/Home/Home'
-import {Navbar} from './components/Navbar/Navbar'
+import { Home } from './pages/Home/Home'
+import { Navbar } from './components/Navbar/Navbar'
 import { SobreMi } from './pages/Sobremi/SobreMi'
-import {Experiencia} from './pages/Experiencia/Experiencia'
-import {Estudios} from './pages/Estudios/Estudios'
-import {Skills} from './pages/Skills/Skills'
-import {Portfolio} from './pages/Portfolio/Portfolio'
-import {Servicios} from './pages/Servicios/Servicios'
-import {Contacto} from './pages/Contacto/Contacto'
-import {HeaderSocials} from './components/HeaderSocials/HeaderSocials'
+import { Experiencia } from './pages/Experiencia/Experiencia'
+import { Estudios } from './pages/Estudios/Estudios'
+import { Skills } from './pages/Skills/Skills'
+import { Portfolio } from './pages/Portfolio/Portfolio'
+import { Servicios } from './pages/Servicios/Servicios'
+import { Contacto } from './pages/Contacto/Contacto'
+import { HeaderSocials } from './components/HeaderSocials/HeaderSocials'
 import { Form } from 'react-router-dom'
 import { ItemListContainer } from './common/ItemListContainer/ItemListContainer'
 import { ItemDetailContainer } from './pages'
+import WOW from 'wowjs';
 
 
-const App = () => {
-    return (
-        <>
-        <BrowserRouter>
-            <Navbar/>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/sobremi' element={<SobreMi />} />
-                <Route path='/experiencia' element={<Experiencia/>} />
-                <Route path='/estudios' element={<Estudios />} />
-                <Route path='/skills' element={<Skills />} />
-                <Route path='/portfolio' element={<Portfolio />} />
-                <Route path='/servicios' element={<Servicios />} />
-                <Route path='/itemlistcontainer' element={<ItemListContainer />} />
-                <Route path='/categoria/:id' element={<ItemListContainer />} />
-                <Route path='/detalle/:id' element={<ItemDetailContainer />} />
-                <Route path='/contacto' element={<Contacto />} />
-            </Routes>
-        </BrowserRouter>
-        <HeaderSocials />
-        </>
+class App extends React.Component {
+    componentDidMount() {
+        new WOW.WOW({
+            live: false
+        }).init();
+    }
+    render() {
+        return (
+            <>
+                <BrowserRouter>
+                    <Navbar />
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/sobremi' element={<SobreMi />} />
+                        <Route path='/experiencia' element={<Experiencia />} />
+                        <Route path='/estudios' element={<Estudios />} />
+                        <Route path='/skills' element={<Skills />} />
+                        <Route path='/portfolio' element={<Portfolio />} />
+                        <Route path='/servicios' element={<Servicios />} />
+                        <Route path='/itemlistcontainer' element={<ItemListContainer />} />
+                        <Route path='/categoria/:id' element={<ItemListContainer />} />
+                        <Route path='/detalle/:id' element={<ItemDetailContainer />} />
+                        <Route path='/contacto' element={<Contacto />} />
+                    </Routes>
+                </BrowserRouter>
+                <HeaderSocials />
+            </>
 
-    )
+        )
+    }
 }
+
 
 export default App
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,25 +3,34 @@ import './home.css'
 import CTA from './CTA'
 import Foto from '../../assets/img/foto-chein.png'
 import Logo from '../../assets/img/navbar-logo.png'
+import WOW from 'wowjs';
 
-const Home = () => {
+
+class Home extends React.Component {
+    componentDidMount() {
+        new WOW.WOW({
+            live: false
+        }).init();
+    }
+    render() {
     return (
         <header id='home'>
             <div className="container header__container">
                 <div className='logo__container'>
-                    <img src={Logo} alt='logo'/>
+                    <img className="wow bounce" data-wow-duration="2s" src={Logo} alt='logo'/>
                 </div>
                 <h5>¡Hola! Mi nombre es</h5>
                 <h1>Ignacio Peña</h1>
                 <h5>Diseñador</h5>
                 <CTA />
-                <div className='foto'>
+                <div className='foto wow bounce' data-wow-duration="2s" >
                     <img src={Foto} alt='foto'/>
                 </div>
             </div>
 
         </header>
     )
+}
 }
 
 export { Home }
